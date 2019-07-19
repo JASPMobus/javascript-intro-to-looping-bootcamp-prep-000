fb2f9dc5a79c83a49d3f4851d7fd0f5277b33525
James Mobus <mobusj@live.com>
2019-07-19 19:31:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,6 +3,5 @@ function forLoop(array) {
     var sOrNot = "s"
     if(i===0) sOrNot = ""
     array.push("I am " + i + "strange loop" + sOrNot + ".")
-    console.log("I am " + i + "strange loop" + sOrNot + ".")
   }
 }
